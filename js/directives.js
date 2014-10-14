2961123582e4d6a52f487816cf64bdddf653d839
mvitalia <p.guglielmo@mvitalia.com>
2014-10-14 15:01:18 +0200

    pp

--- js/directives.js
+++ js/directives.js
@@ -35,3 +35,18 @@ function ($swipe) {
             }
         }
 }]);
+
+app.directive('backButton', function(){
+    return {
+      restrict: 'A',
+ 
+      link: function(scope, element, attrs) {
+        element.bind('click', goBack);
+ 
+        function goBack() {
+          history.back();
+          scope.$apply();
+        }
+      }
+    }
+});
